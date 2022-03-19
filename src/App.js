aaa738a271d5bf749142f70a42c922a160bb10f1
mukasa <52168812+koshi-an@users.noreply.github.com>
2022-03-19 15:23:34 +0900

    Organismの作成 ユーザーカードの作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SeccondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "やまなか",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  mail: "yamanaka@sample.com",
+  phone: "000-0000-0000",
+  company: {
+    name: "やまなか構築代行会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
